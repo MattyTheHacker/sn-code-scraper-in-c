29adb069f64ad04005a739a98cf19653cc810b42
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-09-19 19:18:50 +0100

    fixes

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curl/curl.h>
 
 int main(void) {
-    CURL *curl;
-    CURLcode response;
+    curl_global_init(CURL_GLOBAL_ALL);
 
+    CURL *curl = curl_easy_init();
+
+    if (curl) {
+        printf("Curl initialized\n");
+        curl_easy_setopt(curl, CURLOPT_URL, "https://guildofstudents.com");
+
+        CURLcode response = curl_easy_perform(curl);
+
+        if (response != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(response));
+            curl_easy_cleanup(curl);
+        } else {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response);
+        }
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
 }
-
